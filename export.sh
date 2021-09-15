c99e5b038776de4ba1aa108770e1426afb9c1871
John Chilton <jmchilton@gmail.com>
2021-09-15 17:03:47 -0400

    Re-import with more data and update export script with newer export options.

--- export.sh
+++ export.sh
@@ -10,8 +10,9 @@ fi
 
 # sheed_output='sheet:1N84CziEyW0Z109slrL33cuFt3Wpuu037zogkBMhk-C0'
 gx-tool-db export-tabular \
-    --coverage main --coverage eu \
+    --name --description --model-class --tool-shed --repository-owner --repository-name \
     --training-topics --training-tutorials \
+    --coverage main --coverage eu \
     --test test --test anvil_production --test anvil_edge \
     --label iwc_required --label deprecated --label critical \
     --output "data.tsv"
